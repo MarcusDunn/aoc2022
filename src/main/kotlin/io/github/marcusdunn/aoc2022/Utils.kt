b8c1d9c79463f4d353824a2a36a740f02f7af0fe
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-01 12:46:28 -0800

    made it scale to the moon

--- src/main/kotlin/io/github/marcusdunn/aoc2022/Utils.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/Utils.kt
@@ -1,16 +1,47 @@
 package io.github.marcusdunn.aoc2022
 
-fun <T> Sequence<T>.splitAt(predicate: (T) -> Boolean): Sequence<List<T>> = sequence {
-    val accumulator = mutableListOf<T>()
-    for (element in this@splitAt) {
-        if (predicate(element)) {
-            yield(accumulator.toList())
-            accumulator.clear()
-        } else {
-            accumulator.add(element)
-        }
-    }
-    if (accumulator.isNotEmpty()) {
-        yield(accumulator)
+import java.util.PriorityQueue
+import java.util.Queue
+
+fun <T> Sequence<T>.splitAt(predicate: (T) -> Boolean): Sequence<Sequence<T>> = sequence {
+    val iterator = this@splitAt.iterator()
+    while (iterator.hasNext()) {
+        yield(sequence {
+            while (iterator.hasNext()) {
+                val next = iterator.next()
+                if (predicate(next)) {
+                    break
+                }
+                yield(next)
+            }
+        })
     }
 }
+
+class CapacityRestrictedPriorityQueue<T> private constructor(
+    private val capacity: Int,
+    private val comparator: Comparator<T>,
+    private val inner: PriorityQueue<T>,
+) : Queue<T> by inner {
+    constructor(
+        capacity: Int,
+        comparator: Comparator<T>
+    ) : this(capacity, comparator, PriorityQueue(capacity, comparator))
+
+    companion object;
+    override fun add(element: T) = offer(element)
+    override fun offer(e: T) = if (size < capacity) {
+        inner.offer(e)
+    } else {
+        val head = peek()
+        if (comparator.compare(e, head) > 0) {
+            poll()
+            inner.offer(e)
+        } else {
+            false
+        }
+    }
+}
+
+operator fun <T : Comparable<T>> CapacityRestrictedPriorityQueue.Companion.invoke(capacity: Int) =
+    CapacityRestrictedPriorityQueue<T>(capacity, naturalOrder())
--- src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
@@ -1,31 +1,19 @@
 package io.github.marcusdunn.aoc2022.day01
 
+import io.github.marcusdunn.aoc2022.CapacityRestrictedPriorityQueue
+import io.github.marcusdunn.aoc2022.invoke
 import io.github.marcusdunn.aoc2022.splitAt
 import java.nio.file.Path
 import kotlin.io.path.useLines
 
-fun maxElf(path: Path) = path.useLines { lines ->
-    lines
+fun maxElf(path: Path) = getTop(path, 1)
+
+fun sumTopThree(path: Path) = getTop(path, 3)
+
+private fun getTop(path: Path, count: Int) = path.useLines {
+    it
         .splitAt { it.isBlank() }
         .map { it.sumOf { it.toInt() } }
-        .max()
-}
-
-data class TopThree(val first: Int, val second: Int, val third: Int) {
-    fun add(value: Int) = when {
-        value > first -> TopThree(value, first, second)
-        value > second -> TopThree(first, value, second)
-        value > third -> TopThree(first, second, value)
-        else -> this
-    }
-
-    fun sum() = first + second + third
-}
-
-fun sumTopThree(path: Path) = path.useLines { lines ->
-    lines
-        .splitAt { it.isBlank() }
-        .map { it.sumOf { it.toInt() } }
-        .fold(TopThree(0, 0, 0)) { topThree, value -> topThree.add(value) }
-        .sum()
+        .fold(CapacityRestrictedPriorityQueue<Int>(count)) { q, value -> q.apply { q.add(value) } }
+        .sumOf { it }
 }
