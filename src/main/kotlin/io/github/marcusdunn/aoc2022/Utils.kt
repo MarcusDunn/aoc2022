8efbadfbf14b4ab9f36b4b73164bba97ef8677d6
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-01 01:55:58 -0800

    do I have nothing better to do at 2am

---
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/Utils.kt
@@ -0,0 +1,16 @@
+package io.github.marcusdunn.aoc2022
+
+fun <T> Sequence<T>.splitAt(predicate: (T) -> Boolean): Sequence<List<T>> = sequence {
+    val accumulator = mutableListOf<T>()
+    for (element in this@splitAt) {
+        if (predicate(element)) {
+            yield(accumulator)
+            accumulator.clear()
+        } else {
+            accumulator.add(element)
+        }
+    }
+    if (accumulator.isNotEmpty()) {
+        yield(accumulator)
+    }
+}
--- src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
@@ -1,18 +1,15 @@
 package io.github.marcusdunn.aoc2022.day01
 
-import java.lang.Integer.max
+import io.github.marcusdunn.aoc2022.splitAt
 import java.nio.file.Path
 import kotlin.io.path.useLines
 
 fun maxElf(path: Path) = path.useLines { lines ->
-    lines.fold(0 to 0) { (max, curr), line ->
-        if (line.isBlank()) {
-            max(max, curr) to 0
-        } else {
-            max to curr + line.toInt()
-        }
-    }
-}.let { max(it.first, it.second) }
+    lines
+        .splitAt { it.isBlank() }
+        .map { it.sumOf { it.toInt() } }
+        .max()
+}
 
 data class TopThree(val first: Int, val second: Int, val third: Int) {
     fun add(value: Int) = when {
@@ -26,11 +23,9 @@ data class TopThree(val first: Int, val second: Int, val third: Int) {
 }
 
 fun sumTopThree(path: Path) = path.useLines { lines ->
-    lines.fold(TopThree(0, 0, 0) to 0) { (topThree, curr), line ->
-        if (line.isBlank()) {
-            topThree.add(curr) to 0
-        } else {
-            topThree to curr + line.toInt()
-        }
-    }
-}.let { it.first.add(it.second) }.sum()
+    lines
+        .splitAt { it.isBlank() }
+        .map { it.sumOf { it.toInt() } }
+        .fold(TopThree(0, 0, 0)) { topThree, value -> topThree.add(value) }
+        .sum()
+}
