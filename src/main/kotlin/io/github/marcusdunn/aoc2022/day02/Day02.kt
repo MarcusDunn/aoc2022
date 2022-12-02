dceba6898e8863ccd8542bd748d1a201a1423b85
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-01 21:19:39 -0800

    made code not terrifying to look at

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day02/Day02.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day02/Day02.kt
@@ -3,43 +3,36 @@ package io.github.marcusdunn.aoc2022.day02
 import java.nio.file.Path
 import kotlin.io.path.useLines
 
-fun part1(path: Path): String = path.useLines { lines ->
+fun part2(path: Path) = path.useLines { lines ->
     lines
         .map { it.split(" ") }
         .map { it.first().first() to it.last().first() }
-        .map { score(it) }
+        .map { score(it.first, it.second) }
         .sum()
-        .toString()
 }
 
-fun score(curr: Pair<Char, Char>): Int {
-    val (op, me) = curr
-    return when (me) {
-        // lose
-        'X' -> 0 + when (op) {
-            'A' -> 3
-            'B' -> 1
-            'C' -> 2
-            else -> TODO()
-        }
-        // draw
-        'Y' -> 3 + when (op) {
-            'A' -> 1
-            'B' -> 2
-            'C' -> 3
-            else -> TODO()
-        }
-        // win
-        'Z' -> 6 + when (op) {
-            'A' -> 2
-            'B' -> 3
-            'C' -> 1
-            else -> TODO()
-        }
+fun score(op: Char, me: Char) = when (me) {
+    // lose
+    'X' -> 0 + when (op) {
+        'A' -> 3
+        'B' -> 1
+        'C' -> 2
         else -> TODO()
     }
-}
+    // draw
+    'Y' -> 3 + when (op) {
+        'A' -> 1
+        'B' -> 2
+        'C' -> 3
+        else -> TODO()
+    }
+    // win
+    'Z' -> 6 + when (op) {
+        'A' -> 2
+        'B' -> 3
+        'C' -> 1
+        else -> TODO()
+    }
 
-fun part2(path: Path): String = path.useLines { lines ->
-    TODO()
+    else -> TODO()
 }
--- src/test/kotlin/io/github/marcusdunn/aoc2022/day02/TestDay2.kt
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day02/TestDay2.kt
@@ -6,17 +6,7 @@ import org.junit.jupiter.api.Test
 
 class TestDay2 {
     @Test
-    internal fun `check part 1 sample`() {
-        assertEquals("", part1(Path("src/test/resources/day02/sample.txt")))
-    }
-
-    @Test
-    internal fun `check part 1`() {
-        assertEquals("", part1(Path("src/test/resources/day02/input.txt")))
-    }
-
-    @Test
     internal fun `check part 2`() {
-        assertEquals("", part2(Path("src/test/resources/day02/input.txt")))
+        assertEquals(14184, part2(Path("src/test/resources/day02/input.txt")))
     }
 }
