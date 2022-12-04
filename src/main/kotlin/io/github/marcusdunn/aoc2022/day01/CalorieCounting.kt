70bd42b1fe36134d509cc1600059c57ce0697d42
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-03 17:34:01 -0800

    removed shadowed `it`'s

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day01/CalorieCounting.kt
@@ -10,10 +10,10 @@ fun part1(path: Path) = getTop(path, 1)
 
 fun part2(path: Path) = getTop(path, 3)
 
-private fun getTop(path: Path, count: Int) = path.useLines {
-    it
+private fun getTop(path: Path, count: Int) = path.useLines { lines ->
+    lines
         .splitAt { it.isBlank() }
-        .map { it.sumOf { it.toInt() } }
+        .map { it.sumOf { line -> line.toInt() } }
         .fold(CapacityRestrictedPriorityQueue<Int>(count)) { queue, value -> queue.apply { add(value) } }
         .sum()
 }
