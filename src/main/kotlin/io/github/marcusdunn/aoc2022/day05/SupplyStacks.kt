e3b91000016cd11f47244f2e8440f5be52d7d208
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-05 00:52:13 -0800

    removed duplicate code

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day05/SupplyStacks.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day05/SupplyStacks.kt
@@ -3,7 +3,11 @@ package io.github.marcusdunn.aoc2022.day05
 import java.nio.file.Path
 import kotlin.io.path.readText
 
-fun part1(path: Path): String {
+fun part1(path: Path) = solve(CrateMover9000, path)
+
+fun part2(path: Path) = solve(CrateMover9001, path)
+
+private fun solve(crane: Crane, path: Path): String {
     val (crates, commands) = path.readText().split("\n\n")
     val rowsWithIndex = crates
         .lines()
@@ -27,58 +31,39 @@ fun part1(path: Path): String {
         .map {
             it.split("move", "from", "to").filter { it.isNotBlank() }.map { it.trim().toInt() }
         }
-        .map { (move, from, to) -> Command(move, from, to) }
-        .fold(columns) { acc, command -> command.apply(acc) }
+        .map { (move, from, to) -> crane.createCommand(move, from, to) }
+        .fold(columns) { acc, command -> command.execute(acc) }
         .values
         .map { it.last() }
         .joinToString(separator = "")
 }
 
-fun part2(path: Path): String {
-    val (crates, commands) = path.readText().split("\n\n")
-    val rowsWithIndex = crates
-        .lines()
-        .map { line ->
-            line
-                .chunked(4)
-                .map { if (it.getOrNull(1) == ' ') null else it.getOrNull(1) }
-        }
-    val rows = rowsWithIndex.dropLast(1)
-    val index = rowsWithIndex.last()
-    val columns = index
-        .map { it!!.digitToInt() }
-        .associateWith { idx ->
-            rows
-                .map { it.getOrNull(idx - 1) }
-                .reversed()
-                .filterNotNull()
-        }
-    return commands
-        .lines()
-        .map {
-            it.split("move", "from", "to").filter { it.isNotBlank() }.map { it.trim().toInt() }
-        }
-        .map { (move, from, to) -> Command2(move, from, to) }
-        .fold(columns) { acc, command -> command.apply(acc) }
-        .values
-        .map { it.last() }
-        .joinToString(separator = "")
+fun interface Crane {
+    fun createCommand(amount: Int, from: Int, to: Int): Command
 }
 
-data class Command(private val move: Int, private val from: Int, private val to: Int) {
-    fun apply(columns: Map<Int, List<Char>>): Map<Int, List<Char>> = columns.toMutableMap().apply {
-        val fromCol = get(from)!!
-        val toCol = get(to)!!
-        set(to, toCol + fromCol.takeLast(move).reversed())
-        set(from, fromCol.dropLast(move))
+fun interface Command {
+    fun execute(columns: Map<Int, List<Char>>): Map<Int, List<Char>>
+}
+
+object CrateMover9000 : Crane {
+    override fun createCommand(amount: Int, from: Int, to: Int) = Command { columns ->
+        columns.toMutableMap().apply {
+            val fromCol = get(from)!!
+            val toCol = get(to)!!
+            set(to, toCol + fromCol.takeLast(amount).reversed())
+            set(from, fromCol.dropLast(amount))
+        }
     }
 }
 
-data class Command2(private val move: Int, private val from: Int, private val to: Int) {
-    fun apply(columns: Map<Int, List<Char>>): Map<Int, List<Char>> = columns.toMutableMap().apply {
-        val fromCol = get(from)!!
-        val toCol = get(to)!!
-        set(to, toCol + fromCol.takeLast(move))
-        set(from, fromCol.dropLast(move))
+object CrateMover9001 : Crane {
+    override fun createCommand(amount: Int, from: Int, to: Int) = Command { columns ->
+        columns.toMutableMap().apply {
+            val fromCol = get(from)!!
+            val toCol = get(to)!!
+            set(to, toCol + fromCol.takeLast(amount))
+            set(from, fromCol.dropLast(amount))
+        }
     }
 }
