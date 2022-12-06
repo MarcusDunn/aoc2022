088fe08cc5119ee687327a3c0517722990c0cc13
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-06 10:35:56 -0800

    parameterized

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
@@ -3,18 +3,13 @@ package io.github.marcusdunn.aoc2022.day06
 import java.nio.file.Path
 import kotlin.io.path.readText
 
-fun part1(path: Path) = path
-    .readText()
-    .windowed(4)
-    .withIndex()
-    .filter { (_, str) -> str.toSet().size == 4 }
-    .map { it.index + 4 }
-    .first()
+fun part1(path: Path) = solve(path, 4)
 
-fun part2(path: Path) = path
+fun part2(path: Path) = solve(path, 14)
+
+private fun solve(path: Path, uniqueCount: Int) = path
     .readText()
-    .windowed(14)
+    .windowed(uniqueCount) { it.toSet() }
     .withIndex()
-    .filter { (_, str) -> str.toSet().size == 14 }
-    .map { it.index + 14 }
-    .first()
+    .first { (_, chars) -> chars.size == uniqueCount }
+    .index + uniqueCount
