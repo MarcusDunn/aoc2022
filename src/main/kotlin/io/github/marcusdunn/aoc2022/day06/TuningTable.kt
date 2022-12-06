2ce40ac1537d77b835dcd992027810f40dc732a1
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-05 22:45:13 -0800

    parts

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
@@ -5,10 +5,16 @@ import kotlin.io.path.readText
 
 fun part1(path: Path) = path
     .readText()
-    .asSequence()
+    .windowed(4)
+    .withIndex()
+    .filter { (_, str) -> str.toSet().size == 4 }
+    .map { it.index + 4 }
+    .first()
+
+fun part2(path: Path) = path
+    .readText()
     .windowed(14)
     .withIndex()
-    .filter { (idx, str) -> str.toSet().size == 14 }
+    .filter { (_, str) -> str.toSet().size == 14 }
     .map { it.index + 14 }
-    .onEach { println(it) }
     .first()
--- src/test/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day06/TuningTable.kt
@@ -8,12 +8,23 @@ class TuningTable {
     @Test
     internal fun `check part 1`() {
         val part1 = part1(Path("src/test/resources/day06/input.txt"))
-        assertEquals(0, part1)
+        assertEquals(1582, part1)
     }
 
     @Test
     internal fun `check part 1 sample`() {
         val part1 = part1(Path("src/test/resources/day06/sample.txt"))
-        assertEquals(0, part1)
+        assertEquals(7, part1)
+    }
+    @Test
+    internal fun `check part 2`() {
+        val part2 = part2(Path("src/test/resources/day06/input.txt"))
+        assertEquals(3588, part2)
+    }
+
+    @Test
+    internal fun `check part 2 sample`() {
+        val part2 = part2(Path("src/test/resources/day06/sample.txt"))
+        assertEquals(19, part2)
     }
 }
