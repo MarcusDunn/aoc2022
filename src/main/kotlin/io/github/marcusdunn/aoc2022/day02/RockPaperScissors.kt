d63387b3fb32d26ac5fd54e7cd5e4e059cb51fa8
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-07 13:58:23 -0800

    redid p1

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day02/RockPaperScissors.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day02/RockPaperScissors.kt
@@ -3,7 +3,10 @@ package io.github.marcusdunn.aoc2022.day02
 import java.nio.file.Path
 import kotlin.io.path.useLines
 
-fun part2(path: Path) = path.useLines { lines ->
+fun part1(path: Path) = solve(path, ::scorePart1)
+
+fun part2(path: Path) = solve(path, ::scorePart2)
+fun solve(path: Path, score: (Char, Char) -> Int) = path.useLines { lines ->
     lines
         .map { it.split(" ") }
         .map { it.first().first() to it.last().first() }
@@ -11,22 +14,43 @@ fun part2(path: Path) = path.useLines { lines ->
         .sum()
 }
 
-fun score(op: Char, me: Char) = when (me) {
-    // lose
+fun scorePart1(op: Char, me: Char) = when (op) {
+    'A' -> when (me) {
+        'X' -> 1 + 3
+        'Y' -> 2 + 6
+        'Z' -> 3 + 0
+        else -> throw IllegalArgumentException("must be one of X, Y, or Z")
+    }
+    'B' -> when (me) {
+        'X' -> 1 + 0
+        'Y' -> 2 + 3
+        'Z' -> 3 + 6
+        else -> throw IllegalArgumentException("must be one of X, Y, or Z")
+    }
+    'C' -> when (me) {
+        'X' -> 1 + 6
+        'Y' -> 2 + 0
+        'Z' -> 3 + 3
+        else -> throw IllegalArgumentException("must be one of X, Y, or Z")
+    }
+    else -> throw IllegalArgumentException("must be one of A, B, or C")
+}
+
+fun scorePart2(op: Char, me: Char) = when (me) {
     'X' -> 0 + when (op) {
         'A' -> 3
         'B' -> 1
         'C' -> 2
         else -> throw IllegalArgumentException("must be one of A, B, or C")
     }
-    // draw
+
     'Y' -> 3 + when (op) {
         'A' -> 1
         'B' -> 2
         'C' -> 3
         else -> throw IllegalArgumentException("must be one of A, B, or C")
     }
-    // win
+
     'Z' -> 6 + when (op) {
         'A' -> 2
         'B' -> 3
--- src/test/kotlin/io/github/marcusdunn/aoc2022/day02/RockPaperScissorsTest.kt
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day02/RockPaperScissorsTest.kt
@@ -6,6 +6,16 @@ import org.junit.jupiter.api.Test
 
 class RockPaperScissorsTest {
     @Test
+    fun `check part 1 sample`() {
+        val part1 = part1(Path("src/test/resources/day02/sample.txt"))
+        assertEquals(15, part1)
+    }
+
+    @Test
+    internal fun `check part 1`() {
+        assertEquals(13675, part1(Path("src/test/resources/day02/input.txt")))
+    }
+    @Test
     internal fun `check part 2`() {
         assertEquals(14184, part2(Path("src/test/resources/day02/input.txt")))
     }
