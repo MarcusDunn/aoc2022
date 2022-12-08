1abadaa9fac48596cad0c78f39d7777016773c8b
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-07 22:35:39 -0800

    I no longer hate it

--- src/main/kotlin/io/github/marcusdunn/aoc2022/day08/TreetopTreeHouse.kt
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day08/TreetopTreeHouse.kt
@@ -4,81 +4,64 @@ import io.github.marcusdunn.aoc2022.transpose
 import java.nio.file.Path
 import kotlin.io.path.useLines
 
-fun scoreHorizontal(tree: Int, right: List<Int>, left: List<Int>) =
-    ((right
-        .ifEmpty { return 0 }
-        .takeLastWhile { it < tree }
-        .count().let { if (it == right.size) it else it + 1 }) *
-            (left
-                .ifEmpty { return 0 }
-                .takeWhile { it < tree }
-                .count().let { if (it == left.size) it else it + 1 }))
+fun part1(path: Path): Int = parse(path).let { grid ->
+    buildSet {
+        addHorizontalToSet(grid)
+        addHorizontalToSet(grid.transposeSquare()) { (i, j) -> j to i }
+    }.count()
+}
 
-fun part2(path: Path): Int = path.useLines { lines ->
-    val grid = lines
-        .map { it.map { it.digitToInt() } }
-        .toList()
-    val b = grid
-        .flatMapIndexed { i, row ->
-            row.mapIndexed { j, tree ->
-                (i to j) to scoreHorizontal(
+fun part2(path: Path): Int = parse(path).let { grid ->
+    buildMap {
+        addHorizontalToMap(grid)
+        addHorizontalToMap(grid.transposeSquare()) { (fst, snd) -> snd to fst }
+    }.maxOf { it.value }
+}
+
+private fun List<List<Int>>.transposeSquare() = transpose().map { it.map { it!! } }
+fun parse(path: Path) = path.useLines { lines ->
+    lines.map { it.map { it.digitToInt() } }.toList()
+}
+
+private fun MutableMap<Pair<Int, Int>, Int>.addHorizontalToMap(
+    grid: List<List<Int>>,
+    calcKey: (Pair<Int, Int>) -> Pair<Int, Int> = { it }
+) = grid
+    .forEachIndexed { i, row ->
+        row.forEachIndexed { j, tree ->
+            compute(calcKey(i to j)) { _, left ->
+                (left ?: 1) * scoreHorizontal(
                     tree = tree,
                     right = row.take(maxOf(0, j)),
                     left = row.takeLast(row.size - j - 1)
                 )
             }
-        }.toMap()
-    val a = grid
-        .transpose()
-        .flatMapIndexed { i, row ->
-            row.mapIndexed { j, tree ->
-                (j to i) to scoreHorizontal(
-                    tree = tree!!,
-                    right = row.map { it!! }.take(maxOf(0, j)),
-                    left = row.map { it!! }.takeLast(row.size - j - 1)
+        }
+    }
+
+
+private fun MutableSet<Pair<Int, Int>>.addHorizontalToSet(
+    grid: List<List<Int>>,
+    calcKey: (Pair<Int, Int>) -> Pair<Int, Int> = { it }
+) = grid
+    .forEachIndexed { i, row ->
+        row.forEachIndexed { j, tree ->
+            if (seenHorizontal(
+                    tree = tree,
+                    right = row.map { it }.take(maxOf(0, j)),
+                    left = row.map { it }.takeLast(row.size - j - 1)
                 )
+            ) {
+                add(calcKey(i to j))
             }
         }
-        .toMap()
-    a.mapValues { (k, v) -> v * b[k]!! }.values.max()
+    }
+
+private fun scoreHorizontal(tree: Int, right: List<Int>, left: List<Int>): Int {
+    val rightScore = right.takeLastWhile { it < tree }.count().let { if (it == right.size) it else it + 1 }
+    val leftScore = left.takeWhile { it < tree }.count().let { if (it == left.size) it else it + 1 }
+    return rightScore * leftScore
 }
 
-fun seenHorizontal(tree: Int, right: List<Int>, left: List<Int>) =
-    (right.all { it < tree } || left.all { it < tree })
-
-fun part1(path: Path): Int = path.useLines { lines ->
-    val grid = lines
-        .map { it.map { it.digitToInt() }.withIndex().toList() }
-        .toList()
-        .withIndex()
-        .toList()
-    grid
-        .flatMap { (i, row) ->
-            row.filter { (j, tree) ->
-                seenHorizontal(
-                    tree = tree,
-                    right = row.map { it.value }.take(maxOf(0, j)),
-                    left = row.map { it.value }.takeLast(row.size - j - 1)
-                )
-            }.map { i to it.index }
-        }
-        .toSet()
-        .union(grid
-            .map { it.value.map { it.value } }
-            .transpose()
-            .map { it.withIndex().toList() }
-            .withIndex()
-            .toList()
-            .flatMap { (i, row) ->
-                row.filter { (j, tree) ->
-                    seenHorizontal(
-                        tree = tree!!,
-                        right = row.map { it.value!! }.take(maxOf(0, j)),
-                        left = row.map { it.value!! }.takeLast(row.size - j - 1)
-                    )
-                }.map { it.index to i }
-            }.toSet()
-        )
-        .sortedBy { it.first * 10 + it.second }
-        .count()
-}
+private fun seenHorizontal(tree: Int, right: List<Int>, left: List<Int>) =
+    right.all { it < tree } || left.all { it < tree }
