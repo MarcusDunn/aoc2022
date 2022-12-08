9da5a237219819d2be7e4a877dcde23846007559
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-07 18:34:32 -0800

    setup day 8

---
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day08/Day08.kt
@@ -0,0 +1,12 @@
+package io.github.marcusdunn.aoc2022.day08
+
+import java.nio.file.Path
+import kotlin.io.path.useLines
+
+fun part1(path: Path): Any = path.useLines { lines ->
+    TODO(lines.joinToString())
+}
+
+fun part2(path: Path): Any = path.useLines { lines ->
+    TODO(lines.joinToString())
+}
---
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day08/Day08Test.kt
@@ -0,0 +1,31 @@
+package io.github.marcusdunn.aoc2022.day08
+
+import kotlin.io.path.Path
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+    @Test
+    fun `check part 1 sample`() {
+        val part1 = part1(Path("src/test/resources/day08/sample.txt"))
+        assertEquals(5, part1)
+    }
+
+    @Test
+    fun `check part 1`() {
+        val part1 = part1(Path("src/test/resources/day08/input.txt"))
+        assertEquals(0, part1)
+    }
+
+    @Test
+    fun `check part 2 sample`() {
+        val part2 = part2(Path("src/test/resources/day08/sample.txt"))
+        assertEquals(8, part2)
+    }
+
+    @Test
+    fun `check part 2`() {
+        val part2 = part2(Path("src/test/resources/day08/input.txt"))
+        assertEquals(0, part2)
+    }
+}
