7616dbbdee7298ac2aa2280bffae664f2398a049
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-08 20:22:36 -0800

    prep day 9

---
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day09/Day09.kt
@@ -0,0 +1,12 @@
+package io.github.marcusdunn.aoc2022.day09
+
+import java.nio.file.Path
+import kotlin.io.path.useLines
+
+fun part1(path: Path): Int = path.useLines {
+    TODO()
+}
+
+fun part2(path: Path): Int = path.useLines {
+    TODO()
+}
---
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day09/Day09Test.kt
@@ -0,0 +1,31 @@
+package io.github.marcusdunn.aoc2022.day09
+
+import kotlin.io.path.Path
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day09Test {
+    @Test
+    fun `check part 1 sample`() {
+        val part1 = part1(Path("src/test/resources/day09/sample.txt"))
+        assertEquals(21, part1)
+    }
+
+    @Test
+    fun `check part 1`() {
+        val part1 = part1(Path("src/test/resources/day09/input.txt"))
+        assertEquals(1776, part1)
+    }
+
+    @Test
+    fun `check part 2 sample`() {
+        val part2 = part2(Path("src/test/resources/day09/sample.txt"))
+        assertEquals(8, part2)
+    }
+
+    @Test
+    fun `check part 2`() {
+        val part2 = part2(Path("src/test/resources/day09/input.txt"))
+        assertEquals(234416, part2)
+    }
+}
