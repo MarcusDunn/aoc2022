9e2762cbb176843a2b9927a597d317ac9afa9daa
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-08 20:23:35 -0800

    prep day 9

--- src/test/kotlin/io/github/marcusdunn/aoc2022/day09/Day09Test.kt
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day09/Day09Test.kt
@@ -8,24 +8,24 @@ class Day09Test {
     @Test
     fun `check part 1 sample`() {
         val part1 = part1(Path("src/test/resources/day09/sample.txt"))
-        assertEquals(21, part1)
+        assertEquals(-1, part1)
     }
 
     @Test
     fun `check part 1`() {
         val part1 = part1(Path("src/test/resources/day09/input.txt"))
-        assertEquals(1776, part1)
+        assertEquals(-1, part1)
     }
 
     @Test
     fun `check part 2 sample`() {
         val part2 = part2(Path("src/test/resources/day09/sample.txt"))
-        assertEquals(8, part2)
+        assertEquals(-1, part2)
     }
 
     @Test
     fun `check part 2`() {
         val part2 = part2(Path("src/test/resources/day09/input.txt"))
-        assertEquals(234416, part2)
+        assertEquals(-1, part2)
     }
 }
