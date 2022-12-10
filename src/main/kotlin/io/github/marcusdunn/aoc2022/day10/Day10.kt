6b7f663e1874c54f05f9ffb8cd9c29c1444a9738
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-12-10 12:47:25 -0800

    solved day 10

---
+++ src/main/kotlin/io/github/marcusdunn/aoc2022/day10/Day10.kt
@@ -0,0 +1,71 @@
+package io.github.marcusdunn.aoc2022.day10
+
+import java.nio.file.Path
+import kotlin.io.path.useLines
+
+fun part1(path: Path) = path.useLines { lines ->
+    parse(lines)
+        .toStates()
+        .filter { it.pc >= 20 }
+        .filter { (it.pc - 20) % 40 == 0 }
+        .sumOf { (it.pc) * it.x }
+}
+
+fun part2(path: Path) = path.useLines { lines ->
+    parse(lines)
+        .toStates()
+        .toCrt()
+}
+
+private fun Sequence<State>.toCrt() = this
+    .foldIndexed(StringBuilder()) { i, sb, state ->
+        val crtLoc = i % 40
+        if (crtLoc == 0 && i != 0) {
+            sb.append("\n")
+        }
+        if (crtLoc - state.x in -1..1) {
+            sb.append("#")
+        } else {
+            sb.append(".")
+        }
+        sb
+    }.toString()
+
+private fun Sequence<Instruction>.toStates(): Sequence<State> = sequence {
+    var curr = State(1, 1)
+    for (instruction in this@toStates) {
+        yield(curr)
+        curr = State(curr.pc + 1, instruction.apply(curr.x))
+    }
+    yield(curr)
+}
+
+data class State(val pc: Int, val x: Int)
+
+private fun parse(lines: Sequence<String>) = lines
+    .map { Instruction.parse(it) }
+    .flatMap { List(it.cycles - 1) { Instruction.Noop } + it }
+
+fun Instruction.apply(target: Int) = when (this) {
+    is Instruction.AddX -> target + x
+    Instruction.Noop -> target
+}
+
+
+sealed class Instruction(val cycles: Int) {
+    data class AddX(val x: Int) : Instruction(2)
+    object Noop : Instruction(1)
+
+    companion object {
+        fun parse(line: String) = if (line == "noop") {
+            Noop
+        } else if (line.startsWith("addx")) {
+            AddX(line.split(" ").last().toInt())
+        } else {
+            throw IllegalArgumentException("no instruction for $line")
+        }
+    }
+}
+
+
+
---
+++ src/test/kotlin/io/github/marcusdunn/aoc2022/day10/Day10Test.kt
@@ -0,0 +1,41 @@
+package io.github.marcusdunn.aoc2022.day10
+
+import org.junit.jupiter.api.Test
+import kotlin.io.path.Path
+import kotlin.test.assertEquals
+
+class Day10Test {
+    @Test
+    fun `check part 1 sample0`() {
+        val part1 = part1(Path("src/test/resources/day10/sample0.txt"))
+        assertEquals(0, part1)
+    }
+
+    @Test
+    fun `check part 1 sample`() {
+        val part1 = part1(Path("src/test/resources/day10/sample.txt"))
+        assertEquals(13140, part1)
+    }
+
+    @Test
+    fun `check part 1`() {
+        val part1 = part1(Path("src/test/resources/day10/input.txt"))
+        assertEquals(14720, part1)
+    }
+
+    @Test
+    fun `check part 2`() {
+        val part1 = part2(Path("src/test/resources/day10/input.txt"))
+        assertEquals(
+            """
+            ####.####.###..###..###..####.####.####.
+            #.......#.#..#.#..#.#..#.#.......#.#....
+            ###....#..###..#..#.###..###....#..###..
+            #.....#...#..#.###..#..#.#.....#...#....
+            #....#....#..#.#....#..#.#....#....#....
+            #....####.###..#....###..#....####.#....
+            .""".trimIndent(),
+            part1
+        )
+    }
+}
